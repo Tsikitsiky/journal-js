26abe561af3ebdee218bdf9aafda68b8bace2ae5
Sarah Razanatsoa <sarah.raz@onja.org>
2020-06-12 10:53:39 +0300

    Added refactored journal program

--- journal.js
+++ journal.js
@@ -9,27 +9,30 @@ let listOfEntry = [// An array of oject which stores the journals
 ];
 
 function journalProgram() {
-let homePage = Number(prompt("Welcome to my journal program! \nChoose 1 to show the entry list \n Choose 2 to add a new entry \n Choose 3 to quit"));// Home page asking for what activithy they want to do
+    let homePage = Number(prompt("Welcome to my journal program! \nChoose 1 to show the entry list \nChoose 2 to add a new entry \nChoose 3 to quit"));// Home page asking for what activithy they want to do
 
-if(homePage === 1) {
+    if(homePage === 1) {
+        showList()
+    } else if(homePage === 2) {
+        addingNewJournal();
+    } else {
+        alert("Thank you");
+    }
+    }
+
+function showList() {// showing the list of the existing journal
     for(let i = 0; i < listOfEntry.length; i++) {// Loop through the array and console the journals in it
-        alert(`title: ${listOfEntry[i][0]} \ncontent: ${listOfEntry[i][1]}`);
+        alert(`Title: ${listOfEntry[i][0]} \nContent: ${listOfEntry[i][1]}`);
     }
     journalProgram();
+}
 
-} else if(homePage === 2) {// prompt for the title and content
+function addingNewJournal() {// prompt for the title and content
     let title = prompt("Enter a title:");
     let  content = prompt("Type here the content");
     let newJournal = [title, content]; // new array to add into the list
-    listOfEntry.push(newJournal);
-    for(let i = 0; i < listOfEntry.length; i++) {
-        alert(`title: ${listOfEntry[i][0]} \ncontent: ${listOfEntry[i][1]}`);
-    }
-    journalProgram();
-    
-} else {
-    alert("Thank you");
-}
+    listOfEntry.push(newJournal);// adding the new array to the journal list
+    showList();
 }
 
 journalProgram ();
