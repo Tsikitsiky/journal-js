bbb933413e3dfd58fa18b368e235be9ea30e44f1
Sarah Razanatsoa <sarah.raz@onja.org>
2020-06-15 07:45:05 +0300

    Added the 4th option that deletes the last entry

--- journal.js
+++ journal.js
@@ -9,12 +9,15 @@ let listOfEntry = [// An array of oject which stores the journals
 ];
 
 function startMenu() {
-    let homePage = Number(prompt("Welcome to my journal program! \nChoose 1 to show the entry list \nChoose 2 to add a new entry \nChoose 3 to quit"));// Home page asking for what activithy they want to do
+    let homePage = Number(prompt("Welcome to my journal program! \nChoose 1 to show the entry list \nChoose 2 to add a new entry \nChoose 3 to quit \nChoose 4 to delete the last entry"));// Home page asking for what activithy they want to do
 
     if(homePage === 1) {
         showList()
     } else if(homePage === 2) {
         addingNewJournal();
+    } else if(homePage === 4) {
+        listOfEntry.pop();
+        startMenu();
     } else {
         alert("Thank you");
     }
@@ -32,7 +35,7 @@ function addingNewJournal() {// prompt for the title and content
     let  content = prompt("Type here the content");
     let newJournal = [title, content]; // new array to add into the list
     listOfEntry.push(newJournal);// adding the new array to the journal list
-    showList();
+    startMenu();
 }
 
 startMenu ();
